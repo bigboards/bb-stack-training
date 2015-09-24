b86c74871a1c4edbb849b5f1fe0d893cdb812e99
Daan Gerits <daan@bigboards.io>
2015-09-24 11:34:13 +0200

    adapted the tint metadata and removed the obsolete docker containers

--- update_es.sh
+++ update_es.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -v -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d @tint.json http://hive.bigboards.io:9200/bigboards-hive/library-item/%5Bstack%5Dbigboards%24bb-stack-spark-on-hadoop
-curl -v -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d @stack.json http://hive.bigboards.io:9200/bigboards-hive/stack/%5Bstack%5Dbigboards%24bb-stack-spark-on-hadoop
+curl -v -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d @tint.json http://hive.bigboards.io:9200/bigboards-hive/library-item/%5Bstack%5Dbigboards%24bb-stack-training
+curl -v -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d @stack.json http://hive.bigboards.io:9200/bigboards-hive/stack/%5Bstack%5Dbigboards%24bb-stack-training
