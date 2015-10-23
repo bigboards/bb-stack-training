8c67668626dd7c3ce1071198342de53813e61737
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2015-10-23 11:12:11 +0200

    align tint.json

---
+++ update_hive.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl -v -X POST -u readwrite: -H "Accept: application/json" -H "Content-Type: application/json" -d @tint.json http://hive.test.bigboards.io:9200/bigboards-hive-test/library-item/%5Bstack%5Dgoogle-oauth2-103728492012393057640%24bb-stack-training
