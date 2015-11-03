d1f10d19232fc09d73bc9d8fc55b041c960d89c3
Daan Gerits <daan@bigboards.io>
2015-11-03 09:35:19 +0100

    Added the hive configuration files

--- update_hive.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -v -X POST -u readwrite: -H "Accept: application/json" -H "Content-Type: application/json" -d @tint.json http://hive.test.bigboards.io:9200/bigboards-hive-test/library-item/%5Bstack%5Dgoogle-oauth2-103728492012393057640%24bb-stack-training
